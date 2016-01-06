db17b1d03a7f9bebd4314497ac18af7ea7a2c08a
rkt0589 <rkthakur0589@gmail.com>
2016-01-06 11:22:54 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,9 @@
+// Very Basic Hello world program that can be compiled and run on different system providing compiled with appropriate toolchain(compiler
+
+#include <stdio.h>                  // standard I/O header file
+ 
+int main(int argc, char **argv)
+{
+     printf("\nHello World !!!\n");        // print "Hello World" message on screen
+     return(0);
+}
